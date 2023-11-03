182cfe4a1699fa50f778260905ff5f58ae443b9a
tp2042 <tp2042@hw.ac.uk>
2023-11-03 10:49:15 +0400

    changes

--- week4/main.js
+++ week4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://tp2042.github.io/WP/week4/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
