394953f19da88e6c30f1747bb2255528aecfd2b8
tp2042 <tp2042@hw.ac.uk>
2023-11-03 10:55:23 +0400

    zdcfg

--- week4/main.js
+++ week4/main.js
@@ -2,6 +2,6 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://tp2042.github.io/WP/week4/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
+console.log(ourData);
 };
 ourRequest.send();
